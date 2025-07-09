01703705bb997ab7c9b0322055dc92c3655ea181
Angel Daniel Sanchez Garcia <adanielsanchezgarcia@gmail.com>
2025-07-09 20:42:11 +0200

    FEAT: fix now working when starting outside of playlist

--- scripts/player-image.js
+++ scripts/player-image.js
@@ -1,13 +1,32 @@
+let fixed = false;
+
 function fixCover()
 {
-	let player = document.getElementById("player");
-	let image_wraper = document.getElementById("song-image")
-	let image = image_wraper.getElementsByTagName("img")[0];
+	if (fixed){
+		return ;
+	}
+	const player = document.getElementById("player");
+	const image_wraper = document.getElementById("song-image")
+	const image = image_wraper.getElementsByTagName("img")[0];
 
-	console.log(player, image_wraper, image);
-
+	fixed = true;
 	player.appendChild(image);
 	image_wraper.remove();
 }
 
-fixCover();
+const observer = new MutationObserver(() => {
+  fixCover();
+});
+
+let playing_title;
+
+function titleFinder (){
+	playing_title = document.getElementsByClassName("middle-controls")[0].getElementsByClassName("title")[0];
+	if (playing_title) {
+		observer.observe(playing_title, {childList:true, subtree:true});
+		return;
+	}
+	setTimeout(titleFinder, 1000);
+};
+
+titleFinder();
